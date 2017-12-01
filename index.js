210a926c8e94ab6315a42f0fc3b67c4e699a35f6
marko delgadillo <markoneseven@gmail.com>
2017-11-30 18:44:22 -0800

    #4 Exercises 1 and 2 completed.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const technologies = ['HTML', 'CSS', 'Javascript', 'MongoDB', 'NodeJS']
-const labels = document.querySelector('.labels')
+const jobs = [
+  'Junior frontend developer - HTML5/CSS3/React',
+  'Web developer intern',
+  'Full stack developer - Node.js/React',
+  'Senior backend developer - PHP/Node.js/mySQL',
+  'iOS developer',
+  'Android developer'
+]
+
+const $jobList = document.querySelector('.job-list')
+$jobList.textContent = 'TEST'
+
+const $labels = document.querySelector('.labels')
 
 function label(tech) {
   tech.map(name => {
     console.log(name)
-    let label = document.createElement('div')
-    label.setAttribute('id', name.toLowerCase())
-    label.setAttribute('class', 'label')
-    label.textContent = name
-    labels.appendChild(label)
+    let $label = document.createElement('div')
+    $label.setAttribute('id', name.toLowerCase())
+    $label.setAttribute('class', 'label')
+    $label.textContent = name
+    $labels.appendChild($label)
   })
-  return labels
+  return $labels
 }
 
 label(technologies)
